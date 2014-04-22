e619f6f12ed89b9ac007003fe55c576a46dee234
Raivo Laanemets <raivo@infdot.com>
2014-04-22 12:14:40 +0300

    Bump version

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name('dict_schema').
-version('0.0.1').
+version('0.0.2').
 title('Dict converter and validator').
 author('Raivo Laanemets', 'http://rlaanemets.com/').
 home('https://github.com/rla/dict-schema').
